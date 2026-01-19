6b6b24fe120a8286d1740f82ec94cc40719f5bd8
GUSS <icecle92@gmail.com>
2026-01-20 00:49:30 +0500

    add  stopwatch with start/stop/reset

--- v_1.py
+++ v_1.py
@@ -1,12 +1,17 @@
 import time
+from tkinter import *
 
 
 class Stopwatch:
-    # общее время сессии за день
+
     def __init__(self):
+        # общее время сессии за день
         self.total_time_day = []
+        # флаг вкл./ выкл. секундомер
         self.running = False
+        # начало отсчета
         self.start_time = 0.0
+        # срез времени
         self.total_t = 0.0
 
     def start_t(self):
@@ -24,11 +29,61 @@ class Stopwatch:
             self.total_t += (time.time() - self.start_time)
             # сохранить total_t в 'время' сессии за день
             self.total_time_day.append(self.total_t)
-            # обнулить total_t
-            self.total_t = 0.0
             self.running = False
         elif self.total_t > 0.0:
             # сохранить total_t в 'время' сессии за день
             self.total_time_day.append(self.total_t)
-            self.total_t = 0.0
+        # в любом случае скидываем таймер
+        self.total_t = 0.0
 
+    def get_t(self):
+        if self.running:
+            return time.gmtime(self.total_t + (time.time() - self.start_time))
+        return time.gmtime(self.total_t)
+
+
+# ----GUI----
+sw = Stopwatch()
+
+
+def update_sw():
+    now_time = time.strftime('%H:%M:%S', sw.get_t())
+    label.config(text=now_time)
+    shell_SW.after(200, update_sw)
+
+
+def start():
+    btn_start.pack_forget()
+    btn_pause.pack()
+    btn_reset.place(x=73, y=127)
+    sw.start_t()
+    update_sw()
+
+
+def stop():
+    btn_pause.pack_forget()
+    btn_start.pack()
+    btn_reset.place(x=73, y=127)
+    sw.stop_t()
+
+
+def reset():
+    btn_reset.place_forget()
+    btn_pause.pack_forget()
+    btn_start.pack()
+    sw.reset_t()
+
+
+shell_SW = Tk()
+shell_SW.title("StopWatch")
+shell_SW.geometry('300x220')
+shell_SW.resizable(width=False, height=False)
+label = Label(shell_SW, font=('Times New Roman', 30), text='00:00:00')
+label.pack()
+
+btn_start = Button(shell_SW, width=8, font=('Times New Roman', 25), text='START', bg='light green', command=start)
+btn_pause = Button(shell_SW, width=8, font=('Times New Roman', 25), text='STOP', bg='brown', fg='white', command=stop)
+btn_reset = Button(shell_SW, width=8, font=('Times New Roman', 25), text='RESET', bg='black', fg='white', command=reset)
+btn_start.pack()
+
+shell_SW.mainloop()
